454b58ea80a48f4bad257c8ddc8c3f57b0b24794
JenE87 <engelchen.1408@web.de>
2026-02-11 14:35:11 +0000

    Add app.py, multipage.py, blank page files for all app_pages, and test site for page_predict_electricity_cost

--- app_pages/page_predict_electricity_cost.py
+++ app_pages/page_predict_electricity_cost.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import joblib
+import os
 
 def page_predict_electricity_cost_body():
     
-    st.write("### 🔌 Electricity Cost Predictor")
+    st.write("### 🔌 Electricity Cost Prediction")
 
     st.info(
         "This page allows the user to estimate the expected monthly electricity" \
@@ -11,3 +14,14 @@ def page_predict_electricity_cost_body():
     )
 
     st.write("---")
+
+    version = "v1"
+    model_path=f"outputs/ml_pipeline/electricity_cost/{version}"
+
+    model = joblib.load(f"{model_path}/random_forest_model.pkl")
+    model_features = joblib.load(f"{model_path}/model_features.pkl")
+
+    st.success("Model and feature list loaded successfully.")
+
+    if st.checkbox("Show model features (debug)"):
+        st.write(model_features)
